22f222ab8df58d6958a779f569fa08cac555ca9e
Scott W. Vincent <scott@swvincent.com>
2022-12-02 21:00:45 -0500

    Add project files.

---
+++ SimpleApiKeyAuthExample/Authentication/ApiKeyAuthenticationHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SimpleApiKeyAuthExample.Authentication
+{
+    class ApiKeyAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private const string API_KEY_HEADER = "ApiKey";
+
+        public ApiKeyAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock
+        ) : base(options, logger, encoder, clock)
+        {}
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey(API_KEY_HEADER))
+                return Task.FromResult(AuthenticateResult.Fail("Header Not Found."));
+
+            string apiKeyToValidate = Request.Headers[API_KEY_HEADER];
+
+            string apiKey = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                .Build().GetSection("AppSettings")[API_KEY_HEADER];
+
+            if (apiKeyToValidate != apiKey)
+                return Task.FromResult(AuthenticateResult.Fail("Invalid key."));
+
+            //TODO: Not sure what to do here - creating an arbitrary user for now
+            var pskUser = new IdentityUser("PSK");
+
+            return Task.FromResult(AuthenticateResult.Success(CreateTicket(pskUser)));
+        }
+
+        private AuthenticationTicket CreateTicket(IdentityUser user)
+        {
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return ticket;
+        }
+    }
+}
---
+++ SimpleApiKeyAuthExample/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using SimpleApiKeyAuthExample.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("ApiKey")
+    .AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>("ApiKey", options => { });
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Brrrrrr", "Chilly", "Cool", "Fair to Middlin'", "Warm", "Balmy", "Hot", "Muggy", "Hellfire"
+};
+
+app.MapGet("/weatherforecast", [Authorize] () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
